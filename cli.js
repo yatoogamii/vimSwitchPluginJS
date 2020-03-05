d2df94aca3d9156c5ede70f17166989d22abf393
yatoogamii <bracaval.elias.pro@gmail.com>
2020-03-05 09:35:38 +0100

    clean code

--- cli.js
+++ cli.js
@@ -3,132 +3,69 @@
 const readline = require("readline");
 const homedir = require("os").homedir();
 const fs = require("fs");
+const [, , ...args] = process.argv;
 
 ////////////////////////////////////////////////////////////////////////
-//                               config                               //
+//                               start                                //
 ////////////////////////////////////////////////////////////////////////
 
-// process.stdin.setEncoding("utf-8");
+(async function start() {
+  await checkConfigurationDirExist();
 
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
-
-////////////////////////////////////////////////////////////////////////
-//                          global functions                          //
-////////////////////////////////////////////////////////////////////////
-
-async function checkIfConfigurationDirIsCreated() {
   try {
-    const isExist = await fs.accessSync(
-      `${homedir}/.vimConfiguration`,
-      fs.constants.F_OK
-    );
-  } catch (e) {
-    await createConfigurationDir();
-  }
-}
+    const allConfigName = await fs.readdirSync(`${homedir}/.vimConfiguration`);
 
-async function createConfigurationDir() {
-  try {
-    await fs.mkdirSync(`${homedir}/.vimConfiguration`);
-    console.log("dossier de configuration crée");
+    switch (args[0]) {
+      case "create":
+        await createVimConfiguration(allConfigName);
+        break;
+      case "switch":
+        // switchVimConfiguration();
+        break;
+      case "remove":
+        await removeVimConfiguration(allConfigName);
+        break;
+      case "list":
+        await listVimConfiguration(allConfigName);
+        break;
+      case "-h":
+      case "--help":
+        console.log(helpString);
+        break;
+    }
   } catch (e) {
     console.log(e);
   }
+})();
+
+async function checkConfigurationDirExist() {
+  try {
+    await fs.readdirSync(`${homedir}/.vimConfiguration`);
+  } catch (e) {
+    await fs.mkdirSync(`${homedir}/.vimConfiguration`);
+  }
 }
-
-function checkNameExist() {
-  return args[1];
-}
-
-// async function checkIfBackupDirIsCreated() {
-//   try {
-//     const isExist = await fs.accessSync(
-//       `${homedir}/.vimConfiguration/oldConfig`,
-//       fs.constants.F_OK
-//     );
-//   } catch (e) {
-//     await createBackupDir();
-//   }
-// }
-
-// async function createBackupDir() {
-//   try {
-//     await fs.mkdirSync(`${homedir}/.vimConfiguration/oldConfig`);
-//     console.log("dossier de backup crée");
-//   } catch (e) {
-//     console.log(e);
-//   }
-// }
-
-// async function updateBackupDir() {
-//   try {
-//     const pluggedDirExist = await fs.accessSync(
-//       `${homedir}/.vimConfiguration/oldConfig/plugged`,
-//       fs.constants.F_OK
-//     );
-//     const vimrcExist = await fs.accessSync(
-//       `${homedir}/.vimConfiguration/oldConfig/.vimrc`,
-//       fs.constants.F_OK
-//     );
-
-//     console.log(pluggedDirExist);
-//     console.log(vimrcExist);
-
-//     if (pluggedDirExist) {
-//       await fs.rmdirSync(`${homedir}/.vimConfiguration/oldConfig/plugged`);
-//     }
-//     if (vimrcExist) {
-//       await fs.unlinkSync(`${homedir}/.vimConfiguration/oldConfig/.vimrc`);
-//     }
-
-//     await fs.copyFileSync(
-//       `${homedir}/.vimrc`,
-//       `${homedir}/.vimConfiguration/oldConfig/.vimrc`
-//     );
-
-//     // boucle pour copier tout ce qu'il ya dans .vim/plugged
-//   } catch (e) {
-//     console.log(e);
-//   }
-// }
 
 ////////////////////////////////////////////////////////////////////////
 //                           create section                           //
 ////////////////////////////////////////////////////////////////////////
 
-async function createVimConfiguation() {
-  if (checkNameExist()) {
-    await checkIfConfigurationDirIsCreated();
-
+async function createVimConfiguration(allConfigName) {
+  if (args[1] && !allConfigName.includes(args[1])) {
     try {
-      await fs.mkdirSync(`${homedir}/.vimConfiguration/${args[1]}/plugged`, {
-        recursive: true
-      });
+      await fs.mkdirSync(`${homedir}/.vimConfiguration/${args[1]}`);
       await fs.writeFileSync(
-        `${homedir}/.vimConfiguration/${args[1]}/.vimrc`,
-        ""
+        `${homedir}/.vimConfiguration/${args[1]}/vimrc`,
+        `" name of configuration : #${args[1]} (dont touch)`
       );
+      console.log(`La configuration '${args[1]}' à bien été crée`);
     } catch (e) {
       console.log(e);
     }
-
-    console.log(`
-    new configuration created with name: ${args[1]}
-
-    now you can:
-      - switch to her
-      - remove her
-
-    `);
+  } else if (!args[1]) {
+    console.log("Veuillez indiquer un nom pour la configuration");
   } else {
-    console.log(`
-
-      Veuillez ajouter un nom à la configuration
-
-      `);
+    console.log("Le nom indiqué est déjà utilisé");
   }
 }
 
@@ -136,52 +73,39 @@ async function createVimConfiguation() {
 //                           switch section                           //
 ////////////////////////////////////////////////////////////////////////
 
-async function switchVimConfiguation() {
-  await checkIfConfigurationDirIsCreated();
-  try {
-    console.log("switch");
-  } catch (e) {
-    console.log(e);
-  }
-}
-
 ////////////////////////////////////////////////////////////////////////
 //                           remove section                           //
 ////////////////////////////////////////////////////////////////////////
 
-async function removeVimConfiguation() {
-  await checkIfConfigurationDirIsCreated();
-  try {
-    await fs.rmdirSync(`${homedir}/.vimConfiguration/${args[1]}/plugged`);
-    await fs.unlinkSync(`${homedir}/.vimConfiguration/${args[1]}/.vimrc`);
-    await fs.rmdirSync(`${homedir}/.vimConfiguration/${args[1]}`);
-    console.log(`
-
-      La configuration ${args[1]} a bien été supprimée
-
-      `);
-  } catch (e) {
-    console.log("La configuration que vous voulez effacer n'existe pas");
+async function removeVimConfiguration(allConfigName) {
+  if (allConfigName.includes(args[1])) {
+    try {
+      await fs.rmdirSync(`${homedir}/.vimConfiguration/${args[1]}`, {
+        recursive: true
+      });
+      console.log(`La configuration '${args[1]}' à bien été supprimé`);
+    } catch (e) {
+      console.log(e);
+    }
+  } else if (args[1]) {
+    console.log(`La configuration '${args[1]}' n'existe pas`);
+  } else {
+    console.log("Veuillez indiqué le nom de la configuration à supprimer");
   }
 }
-
 ////////////////////////////////////////////////////////////////////////
 //                            list section                            //
 ////////////////////////////////////////////////////////////////////////
 
-async function listVimConfiguration() {
-  await checkIfConfigurationDirIsCreated();
-  try {
-    const allConfig = await fs.readdirSync(`${homedir}/.vimConfiguration`);
+async function listVimConfiguration(allConfigName) {
+  if (allConfigName.length > 0) {
     console.log(`
-    Configurations actuel:
+    Configurations actuels: \n\t - ${allConfigName.join("\n\t - ")}
     `);
-    allConfig.forEach(element => {
-      console.log(`    - ${element} `);
-    });
-    console.log(` `);
-  } catch (e) {
-    console.log("Vos configurations n'ont pas pu être listé");
+  } else {
+    console.log(`
+      Vous n'avez pas encore de configuration
+      `);
   }
 }
 
@@ -204,28 +128,3 @@ const helpString = `
       Options:
         -h, --help - (Show help)
   `;
-
-////////////////////////////////////////////////////////////////////////
-//                              general                               //
-////////////////////////////////////////////////////////////////////////
-
-const [, , ...args] = process.argv;
-
-switch (args[0]) {
-  case "create":
-    createVimConfiguation();
-    break;
-  case "switch":
-    switchVimConfiguation();
-    break;
-  case "remove":
-    removeVimConfiguation();
-    break;
-  case "list":
-    listVimConfiguration();
-    break;
-  case "-h":
-  case "--help":
-    console.log(helpString);
-    break;
-}
